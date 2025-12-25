34ed00e600eec116cdeac3c3b45c5cb4b9001f70
thfldjqtsms1-sketch <thfldjqtsms1@gmail.com>
2025-12-26 03:16:39 +0900

    Fix: add basename to BrowserRouter for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/rayray-lab-refresh/">
           <Routes>
             <Route path="/" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
